8a350f0fe0a8beb4648822f6252f0900a4a6ccd1
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-12-13 18:36:51 -0300

    UserRepository parte 2

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,29 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
-import { PrismaService } from '../prisma/prisma.service';
+import { UserRepository } from './repository/user.repository';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly userRepository: UserRepository) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { cpf, password, fullName } = createUserDto;
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    return this.prisma.user.create({
-      data: {
-        cpf,
-        password: hashedPassword,
-        fullName,
-      },
-    });
+    return this.userRepository.createUser(cpf, password, fullName);
   }
 
-  async findByEmail(cpf: string): Promise<User | null> {
-    return this.prisma.user.findUnique({ where: { cpf } });
+  async findByCPF(cpf: string): Promise<User | null> {
+    return this.userRepository.findUserByCPF(cpf);
   }
 
   async validatePassword(password: string, hashedPassword: string): Promise<boolean> {
